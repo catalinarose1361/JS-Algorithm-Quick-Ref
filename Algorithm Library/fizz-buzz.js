ecd33de043a7dd071064eb53d2fb23252561cb66
Catalina Williams <catalina@Catalinas-iMac.local>
2021-03-13 08:35:49 -0700

    fizz buzz algorithm added to ref

---
+++ Algorithm Library/fizz-buzz.js
@@ -0,0 +1,27 @@
+// Write code to loop through the array of numbers
+// At each iteration, if a number is evenly divisible by 3 print "Fizz"
+// If a number is evenly divisible by 5 print "Buzz"
+// If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
+// If a number is not divisible by 3 or 5, print the number
+
+var fizzBuzz = function(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var currentNumber = arr[i]
+
+        if (currentNumber % 15 === 0) {
+            console.log("Fizz Buzz")
+        } else if (currentNumber % 3 === 0) {
+            console.log("Fizz")
+        } else if (currentNumber % 5 === 0) {
+            console.log("Buzz")
+        } else {
+            console.log(currentNumber)
+        }
+    }
+};
+
+// % is the modulo operator.
+
+// === means ‘is equal to’
+
+// The 0 means that when var number is divided by 2, there are 0 remainders
